933a418b53af71eb226143f7adc7b26d60ea2435
vikas924 <obroy902@gmail.com>
2023-11-11 18:18:08 +0530

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -16,21 +16,21 @@ ActiveRecord::Schema[7.1].define(version: 2023_07_09_163809) do
 
   create_table "comments", force: :cascade do |t|
     t.text "text"
-    t.bigint "author_id", null: false
+    t.bigint "user_id", null: false
     t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["author_id"], name: "index_comments_on_author_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
-    t.bigint "author_id", null: false
+    t.bigint "user_id", null: false
     t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["author_id"], name: "index_likes_on_author_id"
     t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -47,15 +47,15 @@ ActiveRecord::Schema[7.1].define(version: 2023_07_09_163809) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "photo"
-    t.string "bio"
-    t.integer "posts_counter"
+    t.text "bio"
+    t.integer "posts_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "users", column: "author_id"
+  add_foreign_key "comments", "users"
   add_foreign_key "likes", "posts"
-  add_foreign_key "likes", "users", column: "author_id"
+  add_foreign_key "likes", "users"
   add_foreign_key "posts", "users", column: "author_id"
 end

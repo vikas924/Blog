3673b810b49585c4c20caee9da5930faacc8549e
vikas924 <obroy902@gmail.com>
2023-11-11 18:58:46 +0530

    Create modal and methods for user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ApplicationRecord
   attribute :photo, :string
   attribute :bio, :text
   attribute :posts_counter, :integer, default: 0
+
+  def three_most_recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
 end

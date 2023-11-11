8cdc0deea0ec5e40c6740dfbaef5b0c80ca337d3
vikas924 <obroy902@gmail.com>
2023-11-11 18:49:03 +0530

    Update user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
   has_many :posts, foreign_key: :author_id
-  has_many :comments, foreign_key: :author_id
-  has_many :likes, foreign_key: :author_id
-  has_many :posts, through: :likes, foreign_key: :author_id
-  has_many :posts, through: :comments, foreign_key: :author_id
+  has_many :comments
+  has_many :likes
+
+  attribute :name, :string
+  attribute :photo, :string
+  attribute :bio, :text
+  attribute :posts_counter, :integer, default: 0
 end
